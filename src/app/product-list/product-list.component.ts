9c45e602d55ab19538029585eee50ce43c4e935d
brandiclinard <brandiclinard021911@gmail.com>
2021-09-09 02:00:49 -0500

    Established data passage between parent and child components.

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -11,7 +11,11 @@ export class ProductListComponent {
   products = products;
 
   share() {
-    window.alert('The product has been shared!');
+    window.alert('This product has been shared!');
+  }
+
+  onNotify() {
+    window.alert('You will now be notified when this product goes on sale.')
   }
 }
 
